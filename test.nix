3db41f5e336d38f7e99a45b17b07f758f55f294d
Utku Demir <me@utdemir.com>
2021-09-28 12:16:48 +1300

    Fix test

--- test.nix
+++ test.nix
@@ -1,6 +1,7 @@
 (import ./default.nix).eval
   { pkgs = import <nixpkgs> {}; } ''
-  ((attr pkgs "haskellPackages" "ghcWithPackages")
-     (lambda (ps) __builtins_empty_list)
-  )
+  ;; ((attr pkgs "haskellPackages" "ghcWithPackages")
+  ;;    (lambda (ps) __builtins_empty_list)
+  ;; )
+  (add 1 2)
 ''
