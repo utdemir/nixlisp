3cab29f864517bea8538b8f7e705918c178e35d4
Utku Demir <me@utdemir.com>
2021-09-28 22:30:43 +1300

    Remove deprecated file

--- test.nix
+++
@@ -1,7 +0,0 @@
-(import ./default.nix).eval
-  { pkgs = import <nixpkgs> {}; } ''
-  ;; ((attr pkgs "haskellPackages" "ghcWithPackages")
-  ;;    (lambda (ps) __builtins_empty_list)
-  ;; )
-  (add 1 2)
-''
